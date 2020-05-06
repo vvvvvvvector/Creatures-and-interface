dc27f6d57180b5470898819c50a5451226b1fed9
vvvvvvvector <57532024+vvvvvvvector@users.noreply.github.com>
2020-05-07 01:49:59 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CreaturesAndInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+    }
+}
