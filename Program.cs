e8995dc88b132df834278bec663544e668d28598
vvvvvvvector <57532024+vvvvvvvector@users.noreply.github.com>
2020-05-09 15:37:05 +0300

    refactoring and new feature: now you can create own creature with parts

--- Program.cs
+++ Program.cs
@@ -6,24 +6,123 @@ namespace CreaturesAndInterface
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What would you like to choose: random mode or full creature? 1 - full creature 2 - random creature");
-            var choise = Console.ReadLine();
-            switch (choise)
+            Console.WriteLine("What would You like to choose: random mode, full creature or own creature? \n1 - Full creature \n2 - Random creature \n3 - Own creature");
+            switch (Console.ReadLine())
             {
                 case "1":
-                    Console.WriteLine("What kind of creature would you like to draw: a monster, a bug or a ghost?");
+                    Console.WriteLine("What kind of creature would You like to draw: a monster, a bug or a ghost?");
                     var choice = Console.ReadLine().Replace(" ", "");
+                    Console.WriteLine($"It is a {choice.ToLower()}: ");
                     FullCreature(choice);
                     break;
                 case "2":
+                    Console.WriteLine("It is Your random creature: ");
                     RandomeMode();
                     break;
-                default: 
+                case "3":
+                    Console.WriteLine("Which head do You choose: a monster, a ghost or a bug head? Press \"Enter\" if You don't care");
+                    var head = Console.ReadLine();
+                    Console.WriteLine("Which body do You choose: a monster, a ghost or a bug body? Press \"Enter\" if You don't care");
+                    var body = Console.ReadLine();
+                    Console.WriteLine("Which feet do You choose: a monster, a ghost or a bug feet? Press \"Enter\" if You don't care");
+                    var feet = Console.ReadLine();
+                    Console.WriteLine("It is Your creature: ");
+                    OwnChoiсe(head, body, feet);
+                    break;
+                default:
                     Console.WriteLine("You didn't choose the mode!");
                     break;
             }
 
             Console.ReadLine();
+        }
+        public static void OwnChoiсe(string head, string body, string feet)
+        {
+            var creature = new Creature();
+            switch (head.ToUpper())
+            {
+                case "MONSTER":
+                    creature.Head = new Monster();
+                    break;
+                case "GHOST":
+                    creature.Head = new Ghost();
+                    break;
+                case "BUG":
+                    creature.Head = new Bug();
+                    break;
+                default:
+                    var randomNumber = new Random();
+                    switch (randomNumber.Next(1, 4))
+                    {
+                        case 1:
+                            creature.Head = new Monster();
+                            break;
+                        case 2:
+                            creature.Head = new Ghost();
+                            break;
+                        case 3:
+                            creature.Head = new Bug();
+                            break;
+                    }
+                    break;
+            }
+            switch (body.ToUpper())
+            {
+                case "MONSTER":
+                    creature.Body = new Monster();
+                    break;
+                case "GHOST":
+                    creature.Body = new Ghost();
+                    break;
+                case "BUG":
+                    creature.Body = new Bug();
+                    break;
+                default:
+                    var randomNumber = new Random();
+                    switch (randomNumber.Next(1, 4))
+                    {
+                        case 1:
+                            creature.Body = new Monster();
+                            break;
+                        case 2:
+                            creature.Body = new Ghost();
+                            break;
+                        case 3:
+                            creature.Body = new Bug();
+                            break;
+                    }
+                    break;
+            }
+            switch (feet.ToUpper())
+            {
+                case "MONSTER":
+                    creature.Feet = new Monster();
+                    break;
+                case "GHOST":
+                    creature.Feet = new Ghost();
+                    break;
+                case "BUG":
+                    creature.Feet = new Bug();
+                    break;
+                default:
+                    var randomNumber = new Random();
+                    switch (randomNumber.Next(1, 4))
+                    {
+                        case 1:
+                            creature.Feet = new Monster();
+                            break;
+                        case 2:
+                            creature.Feet = new Ghost();
+                            break;
+                        case 3:
+                            creature.Feet = new Bug();
+                            break;
+                    }
+                    break;
+            }
+            creature.Head.DrawAHead();
+            creature.Body.DrawABody();
+            creature.Feet.DrawAFeet();
         }
         public static void FullCreature(string creature)
         {
